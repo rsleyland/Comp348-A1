6b6ee6eacf2d43a1973007023ce0e95ec7b39c79
p-carrier <p-carrier@users.noreply.github.com>
2020-10-01 12:27:57 -0400

    Shorten the database and add question 3

--- Philippe/Q7.pl
+++ Philippe/Q7.pl
@@ -1,113 +1,63 @@
 % a) Implement the above circuit in PROLOG
 % Database
 
-inv(0, 1)
-inv(1, 0)
+inv(0, 1).
+inv(1, 0).
 
-and(0, 0, 0)
-and(0, 1, 0)
-and(1, 0, 0)
-and(1, 1, 1)
-and(0, 0, 0, 0)
-and(0, 0, 1, 0)
-and(0, 1, 0, 0)
-and(0, 1, 1, 0)
-and(1, 0, 0, 0)
-and(1, 0, 1, 0)
-and(1, 1, 0, 0)
-and(1, 1, 1, 1)
+and([0,0], 0).
+and([0,1], 0).
+and([1,0], 0).
+and([1,1], 1).
+and([X|Y], R) :-
+    and(Y, R1),
+    and([X,R1],R).
 
-or(0, 0, 0)
-or(0, 1, 1)
-or(1, 0, 1)
-or(1, 1, 1)
-or(0, 0, 0, 0)
-or(0, 0, 1, 1)
-or(0, 1, 0, 1)
-or(0, 1, 1, 1)
-or(1, 0, 0, 1)
-or(1, 0, 1, 1)
-or(1, 1, 0, 1)
-or(1, 1, 1, 1)
-or(0, 0, 0, 0, 0)
-or(0, 0, 0, 1, 1)
-or(0, 0, 1, 0, 1)
-or(0, 0, 1, 1, 1)
-or(0, 1, 0, 0, 1)
-or(0, 1, 0, 1, 1)
-or(0, 1, 1, 0, 1)
-or(0, 1, 1, 1, 1)
-or(1, 0, 0, 0, 1)
-or(1, 0, 0, 1, 1)
-or(1, 0, 1, 0, 1)
-or(1, 0, 1, 1, 1)
-or(1, 1, 0, 0, 1)
-or(1, 1, 0, 1, 1)
-or(1, 1, 1, 0, 1)
-or(1, 1, 1, 1, 1)
+or([0,0], 0).
+or([0,1], 1).
+or([1,0], 1).
+or([1,1], 1).
+or([X|Y], R) :-
+    or(Y, R1),
+    or([X,R1], R).
 
-or(0, 0, 0, 0, 0, 0)
-or(0, 0, 0, 0, 1, 1)
-or(0, 0, 0, 1, 0, 1)
-or(0, 0, 0, 1, 1, 1)
-or(0, 0, 1, 0, 0, 1)
-or(0, 0, 1, 0, 1, 1)
-or(0, 0, 1, 1, 0, 1)
-or(0, 0, 1, 1, 1, 1)
-or(0, 1, 0, 0, 0, 1)
-or(0, 1, 0, 0, 1, 1)
-or(0, 1, 0, 1, 0, 1)
-or(0, 1, 0, 1, 1, 1)
-or(0, 1, 1, 0, 0, 1)
-or(0, 1, 1, 0, 1, 1)
-or(0, 1, 1, 1, 0, 1)
-or(0, 1, 1, 1, 1, 1)
-or(1, 0, 0, 0, 0, 1)
-or(1, 0, 0, 0, 1, 1)
-or(1, 0, 0, 1, 0, 1)
-or(1, 0, 0, 1, 1, 1)
-or(1, 0, 1, 0, 0, 1)
-or(1, 0, 1, 0, 1, 1)
-or(1, 0, 1, 1, 0, 1)
-or(1, 0, 1, 1, 1, 1)
-or(1, 1, 0, 0, 0, 1)
-or(1, 1, 0, 0, 1, 1)
-or(1, 1, 0, 1, 0, 1)
-or(1, 1, 0, 1, 1, 1)
-or(1, 1, 1, 0, 0, 1)
-or(1, 1, 1, 0, 1, 1)
-or(1, 1, 1, 1, 0, 1)
-or(1, 1, 1, 1, 1, 1)
+decoder(1,1,1,1,1,1,0,0).
+decoder(0,1,1,0,0,0,0,1).
+decoder(1,1,0,1,1,0,1,2).
+decoder(1,1,1,1,0,0,1,3).
+decoder(0,1,1,0,0,1,1,4).
+decoder(1,0,1,1,0,1,1,5).
+decoder(1,0,1,1,1,1,1,6).
+decoder(1,1,1,0,0,0,0,7).
+decoder(1,1,1,1,1,1,1,8).
+decoder(1,1,1,1,0,1,1,9).
 
 
 % Circuit
-circuit(A, B, C, D, OutA, OutB, OutC, OutD, OutE, OutF, OutG) :-
+circuit(A, B, C, D, Out) :-
     inv(B, InvB),
     inv(C, InvC),
     inv(D, InvD),
-    and(InvB, InvD, And1),
-    and(B, D, And2),
-    and(B, InvD, And3),
-    and(C, D, And4),
-    and(InvC, InvD, And5),
-    and(C, InvD, And6),
-    and(B, InvC, D, And7),
-    and(InvB, C, And8),
-    and(B, InvC, And9),
-    or(A, And1, And2, C, OutA),
-    or(A, InvB, And4, And5, OutB),
-    or(B, D, And5, OutC),
-    or(A, And1, And6, And7, And8, OutD),
-    or(And1, And6, OutE),
-    or(A, And3, And5, And9, OutF),
-    or(A, And6, And8, And9, OutG).
+    and([InvB, InvD], And1),
+    and([B, D], And2),
+    and([B, InvD], And3),
+    and([C, D], And4),
+    and([InvC, InvD], And5),
+    and([C, InvD], And6),
+    and([B, InvC, D], And7),
+    and([InvB, C], And8),
+    and([B, InvC], And9),
+    or([A, And1, And2, C], OutA),
+    or([A, InvB, And4, And5], OutB),
+    or([B, D, And5], OutC),
+    or([A, And1, And6, And7, And8], OutD),
+    or([And1, And6], OutE),
+    or([A, And3, And5, And9], OutF),
+    or([A, And6, And8, And9], OutG),
+    decoder(OutA,OutB,OutC,OutD,OutE,OutF,OutG, Out).
 
-% b) Write the query
-?- circuit(0,1,0,1,OutA,OutB,OutC,OutD,OutE,OutF,OutG)
-  OutA = 1,
-  OutB = 0,
-  OutC = 1,
-  OutD = 1,
-  OutE = 0,
-  OutF = 1,
-  OutG = 1.
+% 2. Write the query
+?- circuit(0,1,0,1,Out)
+  Out = 5
+
+% 3. Explain
+% You follow the schema with the different step and see if at the end you finish at S1, the final state of the Finite State Machine
