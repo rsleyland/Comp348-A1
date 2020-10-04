b868f54331e3cd0e9a12c9c9a916a3b77cdbeec2
rsleyland <71075940+rsleyland@users.noreply.github.com>
2020-10-04 14:32:23 -0400

    Update question5.pl

--- Ryan/question5.pl
+++ Ryan/question5.pl
@@ -2,29 +2,29 @@ teammember(ryan, 40015165).
 teammember(sobhan, 40122438).
 teammember(amr, 40082583).
 teammember(philippe, 40153985).
-student(ryan, comp348, "Principles of Programming Languages").
-student(sobhan, comp348, "Principles of Programming Languages").
-student(amr, comp348, "Principles of Programming Languages").
-student(philippe, comp348, "Principles of Programming Languages").
-student(ryan, comp352, "Data Structures & Algorithms").
-student(sobhan, comp352, "Data Structures & Algorithms").
-student(amr, comp352, "Data Structures & Algorithms").
-student(philippe, comp361, "Elementary Numerical Methods").
-student(ryan, engr391, "Numerical Methods in Engineering").
-student(amr, engr371, "Probability and Statistics").
-student(philippe, encs282, "Technical Writing and Communication").
-student(ryan, engr392, "Impact of Technology on Society").
-student(sobhan, mast218, "Multivariate Calculus I").
-student(amr, phys284, "Introduction to Astronomy").
-student(sobhan, soen287, "Web Programming").
-student(philippe, soen341, "Software Process").
-student(philippe, phys284, "Introduction to Astronomy").
-student(ryan, phys284, "Introduction to Astronomy").
+student(ryan, comp348).
+student(sobhan, comp348).
+student(amr, comp348).
+student(philippe, comp348).
+student(ryan, comp352).
+student(sobhan, comp352).
+student(amr, comp352).
+student(philippe, comp361).
+student(ryan, engr391).
+student(amr, engr371).
+student(philippe, encs282).
+student(ryan, engr392).
+student(sobhan, mast218).
+student(amr, phys284).
+student(sobhan, soen287).
+student(philippe, soen341).
+student(philippe, phys284).
+student(ryan, phys284).
 
 
 %return list of courses taken by each person
 list_courses_student(X,L):-
-  teammember(X,_), findall(Y:Z,student(X,Y,Z),L).
+  teammember(X,_), findall(Y,student(X,Y),L).
 
 %return size of team
 size([],0).
@@ -32,7 +32,7 @@ size([_|T],N):- size(T,N1), N is N1+1.
 team_size(N):-
   findall(X,teammember(X,_),L), size(L,N).
 
-%return all unique courses taken by the team 
+%return all unique courses taken by the team
 list_all_classes(L1) :- findall(X,student(_,X),L), list_to_set(L,L1).
 
 %return previous list sorted using sort/2
@@ -40,4 +40,3 @@ sort_list(L1) :- list_all_classes(L), sort(L,L1).
 
 %unify expression with above result
 unify_list([A,B|C]):- sort_list(X), X = [A,B|C].
-
