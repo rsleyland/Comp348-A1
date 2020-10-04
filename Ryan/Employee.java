3bc14cb4ef409b0ea302d02730cc62ff57d6324a
rsleyland <71075940+rsleyland@users.noreply.github.com>
2020-10-04 10:19:53 -0400

    Update Employee.java

--- Ryan/Employee.java
+++ Ryan/Employee.java
@@ -8,27 +8,11 @@ public class Employee implements Person {
 	Employee(String id, String fn, String ln, String salary){
 		this.id = id; this.firstname = fn; this.lastname = ln; this.salary = salary;
 	}
-	//getters and setters	
 	public String getFirstname() {
 		return firstname;
 	}
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
 	public String getLastname() {
 		return lastname;
-	}
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
-	public String getSalary() {
-		return salary;
-	}
-	public void setSalary(String salary) {
-		this.salary = salary;
-	}
-	public void setId(String id) {
-		this.id = id;
 	}
 	//returns employee info in format id, full name, salary
 	public String toString() {
@@ -42,19 +26,18 @@ public class Employee implements Person {
 			bd = new BigDecimal(salary);
 		}
 		catch(NumberFormatException e) {
-			System.out.println("The salary data for employee with id: \""+this.getId()+"\" does not contain a number.\nPlease fix input file!\nProgram will close.");
+			System.out.println("The salary data for employee with id: \""+this.id+"\" does not contain a number.\nPlease fix input file!\nProgram will close.");
 			System.exit(0);
 		}
 		return bd;
 	}
-	
 	@Override
 	public String getId() {
 		return id;
 	}
 	@Override
 	public String getName() {
-		return (this.getFirstname() +" "+this.getLastname());
+		return (this.firstname +" "+this.lastname);
 	}
 	//splits up csv strings and checks for unwanted characters - return employee object created for string splits
 	public static Person parse(String str) {
@@ -71,11 +54,11 @@ public class Employee implements Person {
 	}
 	//assigns enum values to employee objects - used in stream grouping	
 	public static SalaryRange sortRange(Employee e) {
-		BigDecimal bdSalary = new BigDecimal(e.getSalary());
+		BigDecimal bdSalary = new BigDecimal(e.salary);
 		if (bdSalary.compareTo(BigDecimal.valueOf(25000))<0) return SalaryRange.less25;
 		else if (bdSalary.compareTo(BigDecimal.valueOf(25000))>=0 && bdSalary.compareTo(BigDecimal.valueOf(40000))<0) return SalaryRange.from25to40;
 		else if (bdSalary.compareTo(BigDecimal.valueOf(40000))>=0 && bdSalary.compareTo(BigDecimal.valueOf(70000))<0) return SalaryRange.from40to70;
 		else return SalaryRange.more70;
 	}
-		
+
 }
