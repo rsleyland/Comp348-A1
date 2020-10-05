3b997890ba0b855066716024b62afe533a3e456d
rsleyland <71075940+rsleyland@users.noreply.github.com>
2020-10-05 14:29:12 -0400

    Update Employee.java
    changed toString(), getName() and added enum

--- z_Final/Employee.java
+++ z_Final/Employee.java
@@ -4,13 +4,9 @@ import java.math.BigDecimal;
 public class Employee implements Person {
 	//instance variables
 	private String id = "", firstname = "", lastname = "", salary ="";
-	//public enum salaryRanges {
-	//	LESS25000,
-	//	BETWEEN25000_40000,
-	//	BETWEEN40000_70000,
-	//	OVER70000
-	//}
-	
+	public enum SalaryRange {
+		less25, from25to40, from40to70, more70;
+	}
 	//parameterized constructor
 	Employee(String id, String fn, String ln, String salary){
 		this.id = id; this.firstname = fn; this.lastname = ln; this.salary = salary;
@@ -23,8 +19,7 @@ public class Employee implements Person {
 	}
 	//returns employee info in format id, full name, salary
 	public String toString() {
-		return String.format("%s, %25s %s", id, this.getName()+",", salary);
-		//return (id +", "+this.getName()+", "+salary);
+		return String.format("%s, %25s %s", id, this.firstname+" "+this.lastname+",", salary);
 	}
 	//converts salary string to BigDecimal - try catch ensure the string to be converted is a number
 	public BigDecimal getBigDecSalary() {
@@ -44,7 +39,7 @@ public class Employee implements Person {
 	}
 	@Override
 	public String getName() {
-		return (this.firstname +" "+this.lastname);
+		return (this.lastname +" "+this.firstname);
 	}
 	//splits up csv strings and checks for unwanted characters - return employee object created for string splits
 	public static Person parse(String str) {
@@ -71,21 +66,11 @@ public class Employee implements Person {
 	public static SalaryRange sortRange(Employee e) {
 		BigDecimal bdSalary = new BigDecimal(e.salary);
 		if (bdSalary.compareTo(BigDecimal.valueOf(25000))<0) return SalaryRange.less25;
-		else if (bdSalary.compareTo(BigDecimal.valueOf(25000))>=0 && bdSalary.compareTo(BigDecimal.valueOf(40000))<0) return SalaryRange.from25to40;
-		else if (bdSalary.compareTo(BigDecimal.valueOf(40000))>=0 && bdSalary.compareTo(BigDecimal.valueOf(70000))<0) return SalaryRange.from40to70;
+		else if (bdSalary.compareTo(BigDecimal.valueOf(40000))<0) return SalaryRange.from25to40;
+		else if (bdSalary.compareTo(BigDecimal.valueOf(70000))<0) return SalaryRange.from40to70;
 		else return SalaryRange.more70;
 	}
 	
-	// public salaryRanges getSalaryRange() {
-        //	BigDecimal salary = getSalary();
-        //	if (salary.compareTo(BigDecimal.valueOf(25_000)) < 0)
-        //    		return salaryRanges.LESS25000;
-        //	else if (salary.compareTo(BigDecimal.valueOf(40_000)) < 0)
-        //    		return salaryRanges.BETWEEN25000_40000;
-        //	else if (salary.compareTo(BigDecimal.valueOf(70_000)) < 0)
-        //    		return salaryRanges.BETWEEN40000_70000;
-        //	else
-        //    		return salaryRanges.OVER70000;
     }
 		
 }
